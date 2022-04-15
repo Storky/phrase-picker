a71f7028ad2ac855589a7293f3613b725d950148
Лазарович Виталий Владимирович <VLazarovich@alfabank.ru>
2022-04-15 18:08:31 +0300

    mui5 crash course

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-
+import TourCard from 'components/TourCard';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import SearchAppBar from 'components/AppBar';
+import cities from 'data.json';
+import Typography from '@mui/material/Typography';
 
 
 
@@ -11,12 +15,31 @@ function App() {
     <React.Fragment>
         <CssBaseline />
 
-        <div className="App">
+        <SearchAppBar/>
+        <Container maxWidth="lg" sx={{marginY: 5}}>
 
+            {cities.map((city) => (
+                <React.Fragment>
+                    <Typography
+                        variant={'h4'}
+                        component={'h2'}
+                        marginTop={5}
+                        marginBottom={3}
+                    >
+                        Top {city.name} Tours
+                    </Typography>
 
+                    <Grid container spacing={5}>
+                        {city.tours.map((tour) => (
+                            <TourCard tour={tour} key={tour.name}/>
+                        ))}
+                    </Grid>
+                </React.Fragment>
+            ))}
 
-            <Button variant="contained">Hello World</Button>
-        </div>
+            {/*https://www.youtube.com/watch?v=o1chMISeTC0*/}
+
+        </Container>
     </React.Fragment>
   );
 }
